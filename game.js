adda470a43ba599d324c6bba66fb621ca6339881
Hámori Márk <hamorimark@hotmail.com>
2022-02-09 09:11:36 +0100

    Adding sprites

---
+++ game.js
@@ -0,0 +1,35 @@
+kaboom({
+  global:true,
+  fullscreen: true,
+  scale: 1,
+  debug: true
+})
+
+loadRoot('https://i.imgur.com/')
+loadSprite('link-going-left', 'eiY5zyX.png')
+loadSprite('link-going-right', 'yZIb8O2.png')
+loadSprite('link-going-down', 'tVtlP6y.png')
+loadSprite('link-going-up', 'UkV0we0.png')
+loadSprite('left-wall', 'rfDoaa1.png')
+loadSprite('top-wall', 'QA257Bj.png')
+loadSprite('bottom-wall', 'vWJWmvb.png')
+loadSprite('right-wall', 'SmHhgUn.png')
+loadSprite('bottom-left-wall', 'awnTfNC.png')
+loadSprite('bottom-right-wall', '84oyTFy.png')
+loadSprite('top-left-wall', 'xlpUxIm.png')
+loadSprite('top-right-wall', 'z0OmBd1.jpeg')
+loadSprite('top-door', 'U9nre4n.png')
+loadSprite('left-door', 'okdJNls.png')
+loadSprite('fire-pot', 'I7xSp7w.png')
+loadSprite('lanterns', 'wiSiY09.png')
+loadSprite('slicer', 'c6JFi5Z.png')
+loadSprite('skeletor', 'Ei1VnX8.png')
+loadSprite('kaboom', 'o9WizfI.png')
+loadSprite('stairs', 'VghkL08.png')
+loadSprite('bg', 'u4DVsx6.png')
+
+scene("game", () => {
+
+})
+
+start("game")
