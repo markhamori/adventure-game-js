a67e7c492c579ca095c1076060e3fe697efad462
Márk Hámori <71316482+markhamori@users.noreply.github.com>
2022-02-18 13:47:53 +0100

    Delete LoseScene.js

--- scenes/LoseScene.js
+++
@@ -1,9 +0,0 @@
-export function LoseScene() {
-  scene('lose', ({ score }) => {
-    add([
-      text(score, 32), 
-      origin('center'),  
-      pos(width() / 2, height() / 2),
-    ])
-  })
-}
